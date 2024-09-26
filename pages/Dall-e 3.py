c613d177f3331ffd3ed12055a78ea7b9cc5341f8
EEMI-Quera <kevin.duranty@eemi.com>
2024-09-26 16:24:28 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
 st.title("Dall-e 3")
+
+# 1. Champ de saisie
+user_input = st.text_input("Tapez votre texte")
+
+# 2. Champ de saisie dans la sidebar (pour la clé OpenAI)
+openai_ky = st.sidebar.text_input("Renseignez la clé OpenAI")
+
+# 3. Intéraction avec OpenAI
+
+
+# 4. Affichage de l'image
+

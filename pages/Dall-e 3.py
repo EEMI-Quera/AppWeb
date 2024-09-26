09097715f46094013446fe420f67a08bcd3a79e5
EEMI-Quera <kevin.duranty@eemi.com>
2024-09-26 16:26:57 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -6,10 +6,22 @@ st.title("Dall-e 3")
 user_input = st.text_input("Tapez votre texte")
 
 # 2. Champ de saisie dans la sidebar (pour la clé OpenAI)
-openai_ky = st.sidebar.text_input("Renseignez la clé OpenAI")
+openai_key = st.sidebar.text_input("Renseignez la clé OpenAI")
 
 # 3. Intéraction avec OpenAI
+from openai import OpenAI
 
+client = OpenAI(api_key=openai_key)
+image = client.images.generate(
+    model="dall-e-2",
+    prompt=user_input,
+    size="512x512",
+    quality="standard",
+    n=1,
+)
+
+image_url = image.data[0].url
 
 # 4. Affichage de l'image
+st.image(image_url)
 

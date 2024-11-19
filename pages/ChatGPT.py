63f736c89abbe5b500ce5b4748ef86d092e465e7
EEMI-Quera <kevin.duranty@eemi.com>
2024-11-19 13:03:51 +0100

    Update ChatGPT.py

--- pages/ChatGPT.py
+++ pages/ChatGPT.py
@@ -4,7 +4,7 @@ st.title("ChatGPT - Rédacteur Web")
 
 user_input = st.text_input("Choisissez une thématique")
 
-openai_key = st.sidebar.text_input("Tapez une clé OpenAI")
+openai_key = st.sidebar.text_input("Tapez une clé OpenAI, "sk-")
 
 # Rédacteur Correction
 import os
